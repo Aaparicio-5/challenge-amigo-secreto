01c829202e70e34e55547971fb5a1fb15518d7bb
Aaparicio-5 <aa.aparicio.1992@gmail.com>
2025-03-18 19:47:48 -0600

    añadir README

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>Challenge Amigo Secreto</h1>
+
+Un desafío propuesto por el Programa ONE de Alura.
+
+![alt text](./assets/amigo-secreto-browser.jpg)
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
-
+alert("Para iniciar el juego, escribe los nombres de cada uno de tus amigos y añádelos con el botón Añadir. Al tener la lista con los nombres de tu interés presiona Sortear amigo.");
 function agregarAmigo() {
     let inputAmigo = document.getElementById("amigo"); //Esta línea accede al elemento input
     let nombreAmigo = inputAmigo.value; //Esta línea accede al contenido del input
